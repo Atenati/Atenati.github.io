35bac3654b6c23162fd1486dfaecb171753a6019
Atenati Weber-Morrison <atenati.wm@gmail.com>
2022-02-02 19:43:02 -0400

    Removed nextsetter(), downarrow, fixed nav buttons

--- js/emulatekey.js
+++ js/emulatekey.js
@@ -4,55 +4,27 @@
 // Date: Demeber 10, 2019
 
 function btn1func() {
-    pos = 1;
     document.getElementById(`Home`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
 }
 
 function btn2func() {
-    pos = 2;
     document.getElementById(`Skills`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
 }
 
 function btn3func() {
-    pos = 3;
     document.getElementById(`Work Samples`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
 }
 
 function btn4func() {
-    pos = 4;
     document.getElementById(`Goals`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
 }
 
 function btn5func() {
-    pos = 5;
     document.getElementById(`Resume`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
 }
 
 function btn6func() {
-    pos = 6;
     document.getElementById(`Contact`).scrollIntoView();
-    console.log(`jumped to row1 with pos now at ${pos}`)
-    // nextsetter();
-}
-
-function downarrow() {
-    if(pos === 6) {
-        pos = 0;
-    }
-    pos++;
-    document.getElementById(`row-number-${pos}`).scrollIntoView();
-    console.log(`jumped to page: ${pos} with pos now at ${pos}`)
-    // nextsetter();
 }
 
 document.getElementById('btn1').addEventListener('click', btn1func, false);
@@ -61,4 +33,3 @@ document.getElementById('btn3').addEventListener('click', btn3func, false);
 document.getElementById('btn4').addEventListener('click', btn4func, false);
 document.getElementById('btn5').addEventListener('click', btn5func, false);
 document.getElementById('btn6').addEventListener('click', btn6func, false);
-document.getElementById('imgbtn').addEventListener('click', downarrow, false);
