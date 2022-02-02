76c2222f2fcdff59fe0935757e831100fab6961b
Atenati Weber-Morrison <atenati.wm@gmail.com>
2022-02-02 19:31:15 -0400

    Changed scrolling to GSAP ScrollTrigger

--- js/emulatekey.js
+++ js/emulatekey.js
@@ -3,104 +3,46 @@
 // Author: Atenati Weber-Morrison
 // Date: Demeber 10, 2019
 
-document.addEventListener('wheel', scroll);
-let hovernext = document.getElementById('next');
-
-let pos = 1;
-console.log(`Page: ${pos}`);
-
-function nextsetter() {
-    if(pos === 1) {
-        hovernext.innerHTML = 'Skills';
-    }
-    if(pos === 2) {
-        hovernext.innerHTML = 'Work Samples';
-    }
-    if(pos === 3) {
-        hovernext.innerHTML = 'Goals';
-    }
-    if(pos === 4) {
-        hovernext.innerHTML = 'Resume';
-    }
-    if(pos === 5) {
-        hovernext.innerHTML = 'Contact';
-    }
-    if(pos === 6) {
-        hovernext.innerHTML = 'Home';
-    }
-}
-
-function scroll(e){
-    let scrollPos;
-
-    if (e.wheelChange){
-        scrollPos = e.wheelChange;
-    }else{
-        scrollPos = -1 * e.deltaY;
-    }
-
-    if (scrollPos < 0){
-        if(pos < 7) {
-            if(pos ===  6) {
-                pos = 1;
-                console.log(pos);
-                document.getElementById(`row-number-1`).scrollIntoView();
-            }else {
-                pos++;
-                console.log(`Page: ${pos}`);
-                document.getElementById(`row-number-${pos}`).scrollIntoView();
-            }
-        }
-    }else if (scrollPos > 0) {
-        if (pos > 1) {
-            pos--;
-            console.log(`Page: ${pos}`);
-            document.getElementById(`row-number-${pos}`).scrollIntoView();
-        }
-    }
-    nextsetter();
-}
-
 function btn1func() {
     pos = 1;
-    document.getElementById(`row-number-1`).scrollIntoView();
+    document.getElementById(`Home`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function btn2func() {
     pos = 2;
-    document.getElementById(`row-number-2`).scrollIntoView();
+    document.getElementById(`Skills`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function btn3func() {
     pos = 3;
-    document.getElementById(`row-number-3`).scrollIntoView();
+    document.getElementById(`Work Samples`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function btn4func() {
     pos = 4;
-    document.getElementById(`row-number-4`).scrollIntoView();
+    document.getElementById(`Goals`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function btn5func() {
     pos = 5;
-    document.getElementById(`row-number-5`).scrollIntoView();
+    document.getElementById(`Resume`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function btn6func() {
     pos = 6;
-    document.getElementById(`row-number-6`).scrollIntoView();
+    document.getElementById(`Contact`).scrollIntoView();
     console.log(`jumped to row1 with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 function downarrow() {
@@ -110,7 +52,7 @@ function downarrow() {
     pos++;
     document.getElementById(`row-number-${pos}`).scrollIntoView();
     console.log(`jumped to page: ${pos} with pos now at ${pos}`)
-    nextsetter();
+    // nextsetter();
 }
 
 document.getElementById('btn1').addEventListener('click', btn1func, false);
